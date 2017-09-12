2aed8fd4c0cd9d8019bfdab44125a9bb5b7b93da
Michael Cittar <mcittar@gmail.com>
2017-09-12 10:37:51 -0700

    Change callback on transactioncontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,4 @@ class ApplicationController < ActionController::Base
  
 	# before_action :configure_permitted_parameters, if: :devise_controller?
 
-	# def configure_permitted_parameters
-	#   devise_parameter_sanitizer.for(:sign_up) << :name
-	#   devise_parameter_sanitizer.for(:sign_up) << :provider
-	#   devise_parameter_sanitizer.for(:sign_up) << :uid
-	# end
 end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,5 @@
 class TransactionsController < ApplicationController
-	skip_before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:create]
 
 	def pickup
 		@sale = Sale.find_by!(guid: params[:guid])
